cd842fdbb3d2138314171dfe817199624049468c
AnthonyMacalinao <lorenzanthony.macalinao@benilde.edu.ph>
2021-08-29 14:16:38 +0800

    Added Sample Project

---
+++ Sample Project/src/com/anthonymacalinao/Main.java
@@ -0,0 +1,19 @@
+package com.anthonymacalinao;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    //prints out Hello World
+	//System.out.println("Hello World");
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("What's your name?");
+    String name = scanner.nextLine();
+
+    System.out.println("Hello " + name);
+    }
+}
